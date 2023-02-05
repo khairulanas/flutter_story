217ee6dec1a7101250848b60dd716eaa7393710e
khairul anas <mafiakanan@gmail.com>
2023-02-05 16:24:16 +0700

    interface for local datasource

--- lib/src/core/data/datasource/local_datasource.dart
+++ lib/src/core/data/datasource/local_datasource.dart
@@ -2,42 +2,70 @@ import 'package:flutter_story/src/core/data/model/common_response.dart';
 import 'package:flutter_story/src/core/data/model/login_response.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LocalDatasource {
+abstract class LocalDatasource {
+  Future<CommonResponse> saveLoginData(LoginResult loginData);
+  Future<LoginResult> getLoginData();
+  Future<bool> deleteLoginData();
+  Future<bool> isLoggedIn();
+  String getToken();
+}
+
+class LocalDatasourceImpl implements LocalDatasource {
   final SharedPreferences sharedPreferences;
 
-  LocalDatasource(this.sharedPreferences);
+  LocalDatasourceImpl(this.sharedPreferences);
 
-  Future<CommonResponse> saveLoginData(
-      String userId, String name, String token) async {
-    final isLoginSaved = await sharedPreferences.setBool("isLogin", true);
-    final isUserIdSaved = await sharedPreferences.setString("userId", userId);
-    final isNameSaved = await sharedPreferences.setString("name", name);
-    final isTokenSaved = await sharedPreferences.setString("token", token);
+  final String isLoginKey = "isLogin";
+  final String userIdKey = "isLogin";
+  final String nameKey = "isLogin";
+  final String tokenKey = "isLogin";
+
+  @override
+  Future<CommonResponse> saveLoginData(LoginResult loginData) async {
+    final isLoginSaved = await sharedPreferences.setBool(isLoginKey, true);
+    final isUserIdSaved =
+        await sharedPreferences.setString(userIdKey, loginData.userId);
+    final isNameSaved =
+        await sharedPreferences.setString(nameKey, loginData.name);
+    final isTokenSaved =
+        await sharedPreferences.setString(tokenKey, loginData.token);
     if (isLoginSaved && isUserIdSaved && isTokenSaved && isNameSaved) {
       return CommonResponse(false, "success");
     }
     throw Exception("fail to save data login");
   }
 
-  Future<LoginResult> getLoginData(String name, String token) async {
-    final isLogin = sharedPreferences.getBool("isLogin") ?? false;
+  @override
+  Future<LoginResult> getLoginData() async {
+    final isLogin = sharedPreferences.getBool(isLoginKey) ?? false;
     if (!isLogin) {
       throw Exception("user not logged in");
     }
-    final userId = sharedPreferences.getString("userId");
-    final name = sharedPreferences.getString("name");
-    final token = sharedPreferences.getString("token");
+    final userId = sharedPreferences.getString(userIdKey);
+    final name = sharedPreferences.getString(nameKey);
+    final token = sharedPreferences.getString(tokenKey);
     return LoginResult(userId!, name!, token!);
   }
 
+  @override
   Future<bool> deleteLoginData() async {
-    final isLoginRemoved = await sharedPreferences.remove("isLogin");
-    final isUserIdRemoved = await sharedPreferences.remove("userId");
-    final isNameRemoved = await sharedPreferences.remove("name");
-    final isTokenRemoved = await sharedPreferences.remove("token");
+    final isLoginRemoved = await sharedPreferences.remove(isLoginKey);
+    final isUserIdRemoved = await sharedPreferences.remove(userIdKey);
+    final isNameRemoved = await sharedPreferences.remove(nameKey);
+    final isTokenRemoved = await sharedPreferences.remove(tokenKey);
     if (isLoginRemoved && isUserIdRemoved && isTokenRemoved && isNameRemoved) {
       return true;
     }
     throw Exception("fail to remove data login");
+  }
+
+  @override
+  Future<bool> isLoggedIn() async {
+    return sharedPreferences.getBool(isLoginKey) ?? false;
+  }
+
+  @override
+  String getToken() {
+    return sharedPreferences.getString(tokenKey)!;
   }
 }
