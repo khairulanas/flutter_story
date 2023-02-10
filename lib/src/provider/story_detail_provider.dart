e4a768b41bcda0eba9ef1eba6f14b519e3457286
khairul anas <mafiakanan@gmail.com>
2023-02-10 17:12:26 +0700

    story detail provider

---
+++ lib/src/provider/story_detail_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_story/src/core/common/result_state.dart';
+import 'package:flutter_story/src/core/data/repository.dart';
+import 'package:flutter_story/src/core/domain/entities/story_entity.dart';
+
+class StoryDetailProvider with ChangeNotifier {
+  final Repository repository;
+
+  StoryDetailProvider(this.repository);
+
+  ResultState state = ResultState.loading;
+  late StoryEnity story;
+  String errMessage = '';
+
+  Future<void> getStoryList(String id) async {
+    state = ResultState.loading;
+    notifyListeners();
+
+    final res = await repository.getStoryById(id);
+    res.fold((l) {
+      state = ResultState.error;
+      errMessage = l.message;
+    }, (r) {
+      state = ResultState.sucess;
+      story = r;
+    });
+    notifyListeners();
+  }
+}
