34479d5e3cd44a973b14c4d01b26aee5361f3855
khairul anas <mafiakanan@gmail.com>
2023-02-10 17:10:54 +0700

    register screen v1

--- lib/src/presentation/screen/register_screen.dart
+++ lib/src/presentation/screen/register_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+import 'package:flutter_story/src/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
+
+class RegisterScreen extends StatefulWidget {
   final Function() onRegister;
   final Function() onLogin;
 
@@ -11,25 +14,125 @@ class RegisterScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final emailController = TextEditingController();
+
+  final nameController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    nameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('RegisterScreen'),
+        title: const Text("Register Screen"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text("RegisterScreen"),
-          TextButton(
-            onPressed: onLogin,
-            child: const Text("onLogin"),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 300),
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email.';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: nameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name.';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Name",
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password.';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  context.watch<AuthProvider>().isLoadingRegister
+                      ? const Center(child: CircularProgressIndicator())
+                      : ElevatedButton(
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              final authRead = context.read<AuthProvider>();
+
+                              final result = await authRead.register(
+                                  emailController.text,
+                                  nameController.text,
+                                  passwordController.text);
+                              if (result) {
+                                widget.onRegister();
+                                if (mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text("success login"),
+                                    ),
+                                  );
+                                }
+                              } else {
+                                if (mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(authRead.errMessage ?? ""),
+                                    ),
+                                  );
+                                }
+                              }
+                            }
+                          },
+                          child: const Text("REGISTER"),
+                        ),
+                  const SizedBox(height: 8),
+                  OutlinedButton(
+                    onPressed: () => widget.onLogin(),
+                    child: const Text("LOGIN"),
+                  ),
+                ],
+              ),
+            ),
           ),
-          TextButton(
-            onPressed: onRegister,
-            child: const Text("onRegister"),
-          ),
-        ],
+        ),
       ),
     );
   }
