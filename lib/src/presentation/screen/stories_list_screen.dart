32d5fbb1b979986222543aa8b24ef823820650e2
khairul anas <mafiakanan@gmail.com>
2023-02-10 17:11:17 +0700

    story list screen v1

--- lib/src/presentation/screen/stories_list_screen.dart
+++ lib/src/presentation/screen/stories_list_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_story/src/core/common/result_state.dart';
+import 'package:flutter_story/src/core/domain/entities/story_entity.dart';
+import 'package:flutter_story/src/provider/auth_provider.dart';
+import 'package:flutter_story/src/provider/localization_provider.dart';
+import 'package:flutter_story/src/provider/story_list_provider.dart';
+import 'package:provider/provider.dart';
 
-class StoriesListScreen extends StatelessWidget {
+class StoriesListScreen extends StatefulWidget {
   final Function(String) onTapped;
   final Function() onCreateNewStory;
   final Function() onLogout;
@@ -13,29 +19,91 @@ class StoriesListScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<StoriesListScreen> createState() => _StoriesListScreenState();
+}
+
+class _StoriesListScreenState extends State<StoriesListScreen> {
+  @override
+  void initState() {
+    Future.microtask(() => context.read<AuthProvider>().loadUserData());
+    Future.microtask(() => context.read<StoryListProvider>().getStoryList());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('StoriesListScreen'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text("StoriesListScreen"),
-          TextButton(
-            onPressed: () => onTapped("test"),
-            child: const Text("onTapped(test)"),
-          ),
-          TextButton(
-            onPressed: onCreateNewStory,
-            child: const Text("onCreateNewStory"),
-          ),
-          TextButton(
-            onPressed: onLogout,
-            child: const Text("onLogout"),
-          ),
-        ],
+      drawer: Drawer(
+        child: Consumer2<AuthProvider, LocalizationProvider>(
+            builder: (_, authProv, localizeProv, __) {
+          return ListView(
+            children: [
+              DrawerHeader(child: Text(authProv.userData.name)),
+              ListTile(
+                title: const Text("log out"),
+                onTap: () async {
+                  final isSuccessLogOut = await authProv.logout();
+                  if (isSuccessLogOut) {
+                    widget.onLogout();
+                  }
+                },
+              )
+            ],
+          );
+        }),
+      ),
+      body: Consumer<StoryListProvider>(
+        builder: (_, storyProv, __) {
+          final state = storyProv.state;
+          if (state == ResultState.loading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          } else if (state == ResultState.error) {
+            return Center(
+              child: Text(storyProv.errMessage),
+            );
+          }
+
+          List<StoryEnity> stories = storyProv.listStory;
+          return ListView.builder(
+            itemCount: stories.length,
+            itemBuilder: (context, index) => InkWell(
+              onTap: () {
+                widget.onTapped(stories[index].id);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Image.network(
+                        stories[index].photoUrl,
+                        width: 150,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(stories[index].name),
+                            Text(stories[index].createdAt.toIso8601String())
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
