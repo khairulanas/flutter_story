5e7f59f56fb32a57252022f1e98c41bcbc8ba143
khairul anas <mafiakanan@gmail.com>
2023-02-10 16:15:21 +0700

    init change notfier provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,9 +3,13 @@ import 'package:flutter_story/main.dart';
 import 'package:flutter_story/src/core/data/datasource/local_datasource.dart';
 import 'package:flutter_story/src/core/data/datasource/remote_datasource.dart';
 import 'package:flutter_story/src/core/data/repository.dart';
+import 'package:flutter_story/src/provider/auth_provider.dart';
+import 'package:flutter_story/src/provider/localization_provider.dart';
+import 'package:flutter_story/src/provider/story_list_provider.dart';
 import 'package:flutter_story/src/routes/route_information_parser.dart';
 import 'package:flutter_story/src/routes/router_delegate.dart';
 import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -15,15 +19,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late Repository repository;
+  @override
+  void initState() {
+    repository = RepositoryImpl(
+      RemoteDatasourceImpl(Client()),
+      LocalDatasourceImpl(preferences),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: MyRouteInformationParser(),
-      routerDelegate: MyRouterDelegate(
-        RepositoryImpl(
-          RemoteDatasourceImpl(Client()),
-          LocalDatasourceImpl(preferences),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LocalizationProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider(repository)),
+        ChangeNotifierProvider(create: (_) => StoryListProvider(repository)),
+      ],
+      child: MaterialApp.router(
+        routeInformationParser: MyRouteInformationParser(),
+        routerDelegate: MyRouterDelegate(repository),
+        theme: ThemeData(primaryColor: Colors.black87),
       ),
     );
   }
