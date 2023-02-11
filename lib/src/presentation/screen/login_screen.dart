a05ad7f0ab20cf77a93ddffde416284fa27a803b
khairul anas <mafiakanan@gmail.com>
2023-02-11 15:40:51 +0700

    add package form upload story

--- lib/src/presentation/screen/login_screen.dart
+++ lib/src/presentation/screen/login_screen.dart
@@ -39,9 +39,6 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Center(
         child: ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 300),
-
-          /// todo 16: add Form widget to handle form component, and
-          /// add component key
           child: Form(
             key: formKey,
             child: SingleChildScrollView(
@@ -49,10 +46,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  /// todo 15: add component like TextFormField and Button,
-                  /// add component like controller, hint, obscureText, and onPressed,
-                  /// dispose that controller, and
-                  /// add validation to validate the text.
                   TextFormField(
                     controller: emailController,
                     validator: (value) {
@@ -80,8 +73,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     },
                   ),
                   const SizedBox(height: 8),
-
-                  /// todo 18: update the UI when button is tapped.
                   context.watch<AuthProvider>().isLoadingLogin
                       ? const Center(child: CircularProgressIndicator())
                       : ElevatedButton(
@@ -109,8 +100,6 @@ class _LoginScreenState extends State<LoginScreen> {
                           child: const Text("LOGIN"),
                         ),
                   const SizedBox(height: 8),
-
-                  /// todo 19: update the function when button is tapped.
                   OutlinedButton(
                     onPressed: () => widget.onRegister(),
                     child: const Text("REGISTER"),
