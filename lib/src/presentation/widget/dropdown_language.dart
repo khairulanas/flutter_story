3c2e1c82cdc035362631be86ddd0c926828982a7
khairul anas <mafiakanan@gmail.com>
2023-02-13 15:53:07 +0700

    DropdownLanguage widget

---
+++ lib/src/presentation/widget/dropdown_language.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_story/src/core/common/localization.dart';
+import 'package:flutter_story/src/provider/localization_provider.dart';
+import 'package:provider/provider.dart';
+
+class DropdownLanguage extends StatelessWidget {
+  const DropdownLanguage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LocalizationProvider>(builder: (_, prov, __) {
+      return DropdownButton(
+        value: prov.locale,
+        items: AppLocalizations.supportedLocales.map((Locale locale) {
+          final flag = Localization.getFlag(locale.languageCode);
+          return DropdownMenuItem(
+            value: locale,
+            child: Center(
+              child: Text(
+                flag,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            onTap: () {
+              final provider =
+                  Provider.of<LocalizationProvider>(context, listen: false);
+              provider.setLocale(locale);
+            },
+          );
+        }).toList(),
+        onChanged: (_) {},
+      );
+    });
+  }
+}
