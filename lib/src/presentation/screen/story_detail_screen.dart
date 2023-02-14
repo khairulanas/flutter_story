a8a7f850b1028735dbecd9f9e9504a41c3ba847f
khairul anas <mafiakanan@gmail.com>
2023-02-14 17:31:25 +0700

    StoryDetailScreen v-ai

--- lib/src/presentation/screen/story_detail_screen.dart
+++ lib/src/presentation/screen/story_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_story/main.dart';
+import 'package:flutter_story/src/core/common/date_format.dart';
+import 'package:flutter_story/src/core/common/localization.dart';
 import 'package:flutter_story/src/core/common/result_state.dart';
 import 'package:flutter_story/src/core/data/datasource/local_datasource.dart';
 import 'package:flutter_story/src/core/data/datasource/remote_datasource.dart';
@@ -23,7 +25,7 @@ class StoryDetailScreen extends StatelessWidget {
           storyId),
       child: Scaffold(
         appBar: AppBar(
-          title: const Text(''),
+          title: Text(AppLocalizations.of(context)!.storyDetail),
         ),
         body: Consumer<StoryDetailProvider>(builder: (_, prov, __) {
           final state = prov.state;
@@ -36,25 +38,144 @@ class StoryDetailScreen extends StatelessWidget {
               child: Text(prov.errMessage),
             );
           }
-          return Center(
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                Image.network(prov.story.photoUrl),
-                (prov.story.lat != null && prov.story.lon != null)
-                    ? Row(
-                        children: [
-                          Expanded(child: Text(prov.story.lat.toString())),
-                          Expanded(child: Text(prov.story.lon.toString())),
-                        ],
-                      )
-                    : const SizedBox(),
-                Text(prov.story.name),
-                Text(prov.story.description),
-              ],
-            ),
-          );
+
+          final story = prov.story;
+          return DetailSection(
+              imageUrl: story.photoUrl,
+              name: story.name,
+              description: story.description,
+              date: dateFormat(context, story.createdAt),
+              longitude: story.lon == null ? "-" : story.lon.toString(),
+              latitude: story.lat == null ? "-" : story.lat.toString());
         }),
+      ),
+    );
+  }
+}
+
+class DetailSection extends StatefulWidget {
+  final String imageUrl;
+  final String name;
+  final String description;
+  final String date;
+  final String longitude;
+  final String latitude;
+
+  const DetailSection({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.longitude,
+    required this.latitude,
+  });
+
+  @override
+  State<DetailSection> createState() => _DetailSectionState();
+}
+
+class _DetailSectionState extends State<DetailSection>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeInOut,
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(widget.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SlideTransition(
+              position: _offsetAnimation,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        widget.date,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      widget.name,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      widget.description,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Longitude: ${widget.longitude}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Latitude: ${widget.latitude}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
