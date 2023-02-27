ab8f3377d721e917a99a684a22d9e049b935bc2e
khairul anas <mafiakanan@gmail.com>
2023-02-27 14:36:37 +0700

    add maps configuration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyBQYwtIL4dd0XxVLS5_kn4dHXZRRFMzkSM")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
